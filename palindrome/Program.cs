7bb909e91640249244cbf74aa8b73a7ae1497456
singha5 <arunjitsingh6@gmail.com>
2014-05-05 10:06:55 -0400

    sync

---
+++ palindrome/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter a word:");
+                string word = Console.ReadLine().ToLower();
+
+                if (word == "quit")
+                    break;
+                {
+                    for(int = i; i < word.Length; i++)
+                }
+             
+             } 
+        }
+    }
+}
